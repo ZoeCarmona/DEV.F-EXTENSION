b33805ce4ab1e7ebc23524ff50c424561dfcdd49
Zoe Carmona <zocaya2016@gmail.com>
2026-04-15 10:25:59 -0600

    Add JS data types examples

---
+++ class01/tipos-de-dato.js
@@ -0,0 +1,31 @@
+// Introduction to Different JavaScript Data Types
+
+let number1 = 42;
+const data = 'Veinticinco';
+let number2 = -666;
+let bol = true;
+let number3 = 0;
+let text = '';
+let nothing = null;
+let empty;
+let bolF = false;
+
+// Result: number
+console.log(`1.- ${number1} is ${typeof(number1)}`)
+// Result: string
+console.log(`2.- ${data} is ${typeof(data)}`)
+// Result: number
+console.log(`3.- ${number2} is ${typeof(number2)}`)
+// Result: boolean
+console.log(`4.- ${bol} is ${typeof(bol)}`)
+// Result: number
+console.log(`5.- ${number3} is ${typeof(number3)}`)
+// Result: string
+console.log(`6.- ${text} is ${typeof(text)}`)
+// Result: object
+console.log(`7.- ${nothing} is ${typeof(nothing)}`)
+// Result: undefined
+console.log(`8.- ${empty} is ${typeof(empty)}`)
+// Result: boolean
+console.log(`9.- ${bolF} is ${typeof(bolF)}`)
+
