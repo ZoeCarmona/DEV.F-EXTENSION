72333f25261b41c159ca9782cb5f3d41acacb6d6
Zoe Carmona <zocaya2016@gmail.com>
2026-04-20 19:03:47 -0600

    Add count 10-1 using While

---
+++ class03/whileEx1.js
@@ -0,0 +1,12 @@
+// 1. Crea un ciclo while que cuente del 10 al 1 y lo imprima en la consola.
+
+function countNumbers() {
+    let number = 10;
+
+    while (number !== 0) {
+        console.log(number);
+        number--;
+    }
+}
+
+countNumbers();
